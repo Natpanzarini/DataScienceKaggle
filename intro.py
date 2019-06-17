93b4911a49477d3cd96242ce365202bb55fdddeb
Natalio Panzarini <natpanzarini@Natalios-MacBook-Pro.local>
2019-06-17 13:53:16 -0700

    DS python tools

---
+++ intro.py
@@ -0,0 +1,46 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns  # visualization tool
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+from subprocess import check_output
+print(check_output(["ls", "./input"]).decode("utf8"))
+data = pd.read_csv('./input/pokemon.csv')
+# Any results you write to the current directory are saved as output.
+
+"""
+#correlation map
+f,ax = plt.subplots(figsize=(18, 18))
+sns.heatmap(data.corr(), annot=True, linewidths=.5, fmt= '.1f',ax=ax)
+plt.show()
+"""
+
+"""
+# Line Plot
+# color = color, label = label, linewidth = width of line, alpha = opacity, grid = grid, linestyle = sytle of line
+data.Speed.plot(kind = 'line', color = 'g',label = 'Speed',linewidth=1,alpha = 0.5,grid = True,linestyle = ':')
+data.Defense.plot(color = 'r',label = 'Defense',linewidth=1, alpha = 0.5,grid = True,linestyle = '-.')
+plt.legend(loc='upper right')     # legend = puts label into plot
+plt.xlabel('x axis')              # label = name of label
+plt.ylabel('y axis')
+plt.title('Line Plot')            # title = title of plot
+plt.show()
+"""
+
+"""
+# Scatter Plot
+# x = attack, y = defense
+data.plot(kind='scatter', x='Attack', y='Defense',alpha = 0.5,color = 'red')
+plt.xlabel('Attack')              # label = name of label
+plt.ylabel('Defence')
+plt.title('Attack Defense Scatter Plot')            # title = title of plot
+plt.show()
+"""
+data.head()
